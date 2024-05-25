fce57c8b38ef3ae5186c257ad83c7522b1e0cefe
frutillitaaaa <119687288+frutillitaaaa@users.noreply.github.com>
2024-05-25 14:53:29 -0400

    Update hashmap

---
+++ src/HashMap.cpp
@@ -0,0 +1,29 @@
+#include "HashMap.h"
+
+bool HashMap::isEmpty() const
+{
+    
+    return false;
+}
+
+int HashMap::hashFunction(int clave)
+{
+    return 0;
+}
+
+void HashMap::insertarItem(int clave, string valor)
+{
+}
+
+void HashMap::eliminarItem(int clave)
+{
+}
+
+string HashMap::buscarItem(int clave)
+{
+    return string();
+}
+
+void HashMap::desplegarMap()
+{
+}
